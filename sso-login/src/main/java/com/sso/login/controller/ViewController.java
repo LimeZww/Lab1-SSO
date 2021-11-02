3a65ff50b42499222eabd971fbb2899dfbf7074c
郑维维 <2991037657@qq.com>
2021-11-02 17:05:21 +0800

    系统主页修改

--- sso-login/src/main/java/com/sso/login/controller/ViewController.java
+++ sso-login/src/main/java/com/sso/login/controller/ViewController.java
@@ -23,8 +23,8 @@ public class ViewController {
     public String toIndex(@RequestParam(required = false,defaultValue = "")String target,
                           HttpSession session,@CookieValue(required = false,value = "TOKEN") Cookie cookie){
         if(StringUtils.isEmpty(target)){
-            //默认跳转到系统A
-            target="http://systemC.codeshop.com:9011";
+            //默认跳转到系统C
+            target="http://systemC.codeshop.com:9011/view/index";
         }
         //如果已经登录的用户再次访问登录系统时，就要重定向
         if(cookie != null){
